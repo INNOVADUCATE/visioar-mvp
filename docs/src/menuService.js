ac297cc1a2d39c71d329594bf225e2b751369160
INNOVADUCATE <innovaducate@gmail.com>
2026-01-27 13:52:42 -0300

    Remove project files and documentation
    Deleted workflow configurations, documentation, interface assets, and all source files from the project, including the README and MVP notes. This cleans the repository of all previous content.

--- docs/src/menuService.js
+++
@@ -1,8 +0,0 @@
-export async function loadMenu() {
-  const url = new URL("../menu.json", import.meta.url);
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error("No se pudo cargar menu.json");
-  }
-  return response.json();
-}
